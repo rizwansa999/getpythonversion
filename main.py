cb6fcc2ecee696c7fe35dcc6290c754d91bd3dc5
rizwansa999 <58390860+rizwansa999@users.noreply.github.com>
2021-12-29 23:14:01 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+import sys
+
+print("Python Version : "+ str(sys.version))
+print("Version Info : " + str(sys.version_info))
+print("Release : " + str(sys.version_info[0]) + "." + str(sys.version_info[1]))
